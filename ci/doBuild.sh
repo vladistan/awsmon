a736f4e69d16f147e28a3aa58a1df6967bac86f5
Vlad <vlad@v-lad.org>
2017-11-23 08:47:59 -0800

    Fixup ci script

--- ci/doBuild.sh
+++ ci/doBuild.sh
@@ -14,5 +14,9 @@ fi
 echo Using UID ${USE_UID}
 
 echo Building aws_mon_robot
-docker run -u ${USE_UID} -v $(pwd):/app --entrypoint /bin/bash  local/aws_mon_builder /app/ci/build-and-test.sh
+docker run -u ${UID:-1001}:${UID:-1001} \
+       -v $(pwd):/app \
+       --entrypoint /bin/bash \
+       local/aws_mon_builder \
+       /app/ci/build-and-test.sh
 

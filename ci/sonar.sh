57a4fad9d6a1eda9478969a109d205705411c2d8
Vlad Korolev <vlad@appliedbiomath.com>
2021-03-13 20:55:14 -0800

    Fix for new build server

--- ci/sonar.sh
+++ ci/sonar.sh
@@ -18,9 +18,13 @@ mkdir -p .m2
 mkdir -p .sonar
 mkdir -p .gradle
 
+echo "Sonar Host ${SONAR_HOST_URL}"
+echo "Sonar User ${SONAR_LOGIN}"
+
 docker run -u $USE_UID:$USE_UID \
     -e TZ=America/New_York \
     -e HOME=/awsmon \
+    -e JAVA_OPTS="-Dsonar.host.url=${SONAR_HOST_URL} -Dsonar.login=${SONAR_LOGIN} -Dsonar.password=${SONAR_PASSWORD}" \
     -v $(pwd):/awsmon \
     -v ${HOME}/.m2:/awsmon/?/.m2:rw \
     -v ${HOME}/.m2:/awsmon/.m2 \
